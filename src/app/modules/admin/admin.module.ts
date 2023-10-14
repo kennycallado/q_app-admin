71b894ee7ac866880c46a37148621c854b53b623
kennycallado <kennycallado@hotmail.com>
2023-10-14 20:42:37 +0200

    ADD Inicializa módulo admin con project

---
+++ src/app/modules/admin/admin.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ProjectComponent } from './components/project/project.component';
+
+
+@NgModule({
+  declarations: [
+    AdminComponent,
+    SidebarComponent,
+    ProjectComponent,
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    RouterModule
+  ]
+})
+export class AdminModule { }
