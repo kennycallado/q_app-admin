3c4042b0a6f1632f2682f660a50545c1ab20db6c
kennycallado <kennycallado@hotmail.com>
2023-10-25 16:36:49 +0200

    UPDATE Actualiza y activa guard para rutas bajo admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TokenComponent } from './modules/admin/components/token/token.component';
+import { authGuard } from './providers/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'token', component: TokenComponent },
-  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) },
+  { path: 'admin',
+    loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule),
+    canActivate: [authGuard]
+  },
 ];
 
 @NgModule({
--- src/app/providers/guards/auth.guard.ts
+++ src/app/providers/guards/auth.guard.ts
@@ -1,13 +1,13 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  // const authSvc = inject(AuthService)
-  // const router  = inject(Router)
+export const authGuard: CanActivateFn = (_route, state) => {
+  const authSvc = inject(AuthService)
+  const router  = inject(Router)
 
-  // if (authSvc.access_token !== '') return true
+  if (authSvc.access_token !== '') return true
 
-  // router.navigate(['/login'], { queryParams: { returnUrl: state.url } })
-  // return false
-  return true
+  router.navigate(['/login'], { queryParams: { returnUrl: state.url } })
+  return false
 };
