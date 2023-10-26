c41d7e919fd569a8fd607da422d446933e46084b
kennycallado <kennycallado@hotmail.com>
2023-10-26 09:18:44 +0200

    CLEAN

--- src/app/providers/constants.ts
+++ src/app/providers/constants.ts
@@ -4,11 +4,10 @@ export const VAPID_PUBLIC_KEY = "BEiXD93a1MhsHGNdnTYChocRr9I58XvO9kpZwMNvZc9QJFG
 export const BASE_URL = 'https://questions.kennycallado.dev/'
 const API_VERSION = 'api/v1/'
 
-export const AUTH_URL = BASE_URL + 'auth/'
-export const USER_URL = BASE_URL + API_VERSION + 'user/'
-
-export const MESSAGE_URL = BASE_URL + API_VERSION + 'messaging/'
-
-export const PAPER_URL = BASE_URL + API_VERSION + 'paper/'
+export const AUTH_URL     = BASE_URL + 'auth/'
+export const MESSAGE_URL  = BASE_URL + API_VERSION + 'messaging/'
+export const PAPER_URL    = BASE_URL + API_VERSION + 'paper/'
+export const PROJECT_URL  = BASE_URL + API_VERSION + 'project/'
+export const USER_URL     = BASE_URL + API_VERSION + 'user/'
 
 export const DEFAULT_PICTURE_URL = 'https://picsum.photos/200/300';
