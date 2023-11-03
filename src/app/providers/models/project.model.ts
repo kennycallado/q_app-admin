404574f1a0e224ef64928d090cd6dfa02de0a4a1
kennycallado <kennycallado@hotmail.com>
2023-11-03 11:11:07 +0100

    ADD Agrega modelo record y actualiza project para contenerlo

--- src/app/providers/models/project.model.ts
+++ src/app/providers/models/project.model.ts
@@ -1,2 +1,8 @@
+import { PubRecord } from "./record.model";
+
 export class Project {
+  id: number;
+  name: string;
+  keys: string[];
+  records?: PubRecord[]
 }
---
+++ src/app/providers/models/record.model.ts
@@ -0,0 +1,5 @@
+export class PubRecord {
+  id: number;
+  user_id: number;
+  records: Record<string, string|number>[];
+}
